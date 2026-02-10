c392576815f738bf483e18ec9f263d9369bd37a6
dhruvvd <dhruv.dhananjay7@gmail.com>
2026-02-10 16:20:29 -0500

    base class for metrics calculations; need to see the output of backtrader to better understand how to do the calculations

--- metrics.py
+++ metrics.py
@@ -1,83 +1,23 @@
 import pandas as pd
 import numpy as np
 
-import os
 
-import base64
-import requests
-import webbrowser
-from flask import Request
-from loguru import logger
-from dotenv import load_dotenv
+class MetricCalculator():
+    def cagr(starting_eq, ending_eq, length_of_strat):
+        years = length_of_strat / 365
+        return np.pow(ending_eq / starting_eq, 1 / years) - 1
 
-load_dotenv()
+    def sharpRatio(mean_ret, rf_rate, ret_std):
+        return (mean_ret - rf_rate) / ret_std
 
-def construct_init_auth_url() -> tuple[str, str, str]:
-    
-    app_key = os.getenv("APP_KEY")
-    app_secret = os.getenv("APP_SECRET")
+    def sortinoRatio(mean_ret, rf_rate, retdo_std):
+        return (mean_ret - rf_rate) / retdo_std
 
-    auth_url = f"https://api.schwabapi.com/v1/oauth/authorize?client_id={app_key}&redirect_uri=https://127.0.0.1"
+    def hitRate():
+        pass
 
-    logger.info("Click to authenticate:")
-    logger.info(auth_url)
+    def turnover():
+        pass
 
-    return app_key, app_secret, auth_url
-
-
-def construct_headers_and_payload(returned_url, app_key, app_secret):
-    response_code = f"{returned_url[returned_url.index('code=') + 5: returned_url.index('%40')]}@"
-
-    credentials = f"{app_key}:{app_secret}"
-    base64_credentials = base64.b64encode(credentials.encode("utf-8")).decode(
-        "utf-8"
-    )
-
-    headers = {
-        "Authorization": f"Basic {base64_credentials}",
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-
-    payload = {
-        "grant_type": "authorization_code",
-        "code": response_code,
-        "redirect_uri": "https://127.0.0.1",
-    }
-
-    return headers, payload
-
-
-def retrieve_tokens(headers, payload) -> dict:
-    init_token_response = requests.post(
-        url="https://api.schwabapi.com/v1/oauth/token",
-        headers=headers,
-        data=payload,
-    )
-
-    init_tokens_dict = init_token_response.json()
-
-    return init_tokens_dict
-
-
-def main():
-    app_key, app_secret, cs_auth_url = construct_init_auth_url()
-    webbrowser.open(cs_auth_url)
-
-    logger.info("Paste Returned URL:")
-    returned_url = input()
-
-    init_token_headers, init_token_payload = construct_headers_and_payload(
-        returned_url, app_key, app_secret
-    )
-
-    init_tokens_dict = retrieve_tokens(
-        headers=init_token_headers, payload=init_token_payload
-    )
-
-    logger.debug(init_tokens_dict)
-
-    return "Done!"
-
-if __name__ == "__main__":
-  main()
-
+    def maxDrawdown(df):
+        pass
