6e2204f51776f98a2f742610b9a6017b525e1936
dhruvvd <dhruv.dhananjay7@gmail.com>
2026-02-10 23:47:32 -0500

    wrote in more methods like mean_return, std_return, and downside deviation instead of having to pre calculate those. we take in OHLCV data instead. we can update in future for non buy-hold strats.

--- metrics.py
+++ metrics.py
@@ -1,17 +1,62 @@
 import pandas as pd
 import numpy as np
 
+"""
+--- PREREQUISITES FOR USING THE METRICS CLASS -- 
+
+    You must calculate the following before using any of the Metrics Calculator class methods:
+
+    -> starting equity
+    -> final equity
+"""
+
 
 class MetricCalculator():
-    def cagr(starting_eq, ending_eq, length_of_strat):
-        years = length_of_strat / 365
+    def __init__(self, price_data, rf_annual):
+        self.returns = price_data.pct_change.dropna()
+        self.rf_annual = rf_annual
+
+        self.rf_daily = (1 + self.rf_annual)**(1/252) - 1
+
+    def mean_return(self, annualize):
+        mean = self.returns.mean()
+        
+        if annualize:
+            return mean * 252
+        
+        return mean
+    
+    def std_return(self, annualize):
+        daily_std = self.returns.std()
+
+        if annualize:
+            return daily_std * np.sqrt(252)
+        
+        return daily_std
+
+    def downside_deviation(self, annualize):
+        negative_returns = self.returns.copy()
+        negative_returns[negative_returns > 0] = 0
+        
+        std = negative_returns.std()
+        
+        if annualize:
+            return std * np.sqrt(252)
+        return std
+    
+    def cagr(self, starting_eq, ending_eq, length_of_strat):
+        years = length_of_strat / 252
         return np.pow(ending_eq / starting_eq, 1 / years) - 1
 
-    def sharpRatio(mean_ret, rf_rate, ret_std):
-        return (mean_ret - rf_rate) / ret_std
+    def sharpRatio(self):
+        excess_rets = self.mean_returns(annualize=False) - self.rf_daily
 
-    def sortinoRatio(mean_ret, rf_rate, retdo_std):
-        return (mean_ret - rf_rate) / retdo_std
+        return excess_rets / self.std_return(annualize=False)
+
+    def sortinoRatio(self):
+        excess_rets = self.mean_returns(annualize=False) - self.rf_daily
+
+        return excess_rets / self.downside_deviation(annualize=False)
 
     def hitRate():
         pass
