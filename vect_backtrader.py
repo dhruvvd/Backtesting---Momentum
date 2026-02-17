7e20d51480d0cb81bdbb6ac16b1c7ae710e84509
dhruvvd <dhruv.dhananjay7@gmail.com>
2026-02-16 23:40:44 -0500

    generated signals as well as calculating pnl

--- vect_backtrader.py
+++ vect_backtrader.py
@@ -26,14 +26,43 @@ df.set_index(inplace=True, keys="datetime")
 
 # -- INDICATOR GENERATION -- 
 analysis = pd.DataFrame()
+analysis.index = df.index
 
 analysis['returns'] = df['close'].pct_change()
 analysis['EMA_20'] = df['close'].ewm(span=20, adjust=False).mean()
 analysis['SMA_50'] = df['close'].rolling(50).mean()
+analysis['crossover'] = analysis['EMA_20'] - analysis['SMA_50']
 
 analysis.dropna(inplace=True)
 
-# -- BUY / SELL LOGIC --
+# -- SIGNAL GENERATOR LOGIC --
 
-def buy_order(df):
-    pass
+def signal_gen(df):
+    df['signal'] = 0
+    df['c_shifted'] = df['crossover'].shift(1)
+    df.dropna(inplace=True)
+
+    df[df['c_shifted'] < 0 & df['crossover'] > 0, 'signal'] = 1
+    df[df['c_shifted'] > 0 & df['crossover'] < 0, 'signal'] = 2
+
+
+# -- CALCULATING PROFITS / LOSSES
+
+def calc_pl(signals, price_data, size, commission):
+    buys = pd.Series()
+    sells = pd.Series()
+    combined = signals.join(price_data)
+
+    combined['o_shifted'] = combined['open'].shift(-1)
+
+    buys = combined[combined['signal'] == 1, 'o_shifted']
+    sells = combined[combined['signal'] == 2, 'close']
+
+    commission_b = commission * buys
+    commission_s = commission * sells
+
+    total_commission = commission_b + commission_s
+
+    perc_pnl = ((sells - buys) / buys) * 100
+    pnl = sells - buys
+    return pnl, perc_pnl, total_commission
